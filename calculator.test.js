e40dd66800a9ed0f94f2d2a56a48a07d6430f10b
apple <apple@apples-macbook-air-5.home>
2024-05-12 22:03:50 +0200

    code and test added

--- calculator.test.js
+++ calculator.test.js
@@ -7,7 +7,7 @@ test('string with a single number should result in the number itself', () => {
   test('string with two numbers separated by comma should result in the sum of the numbers', () => {
     expect(calculator.add('4,5')).toBe(9);
   });
-  test('string with three numbers separated by comma should result in the sum of the numbers', () => {
+  test('string with three numbers separated by comma should result in the sum of the numbers.', () => {
     expect(calculator.add('2,8,4')).toBe(14);
 });
 
